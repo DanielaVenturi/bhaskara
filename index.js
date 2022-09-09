424c637a7cc72b4c1c72d2dce4a7f8ff6b64c0b3
DanielaVenturi <danielaventuri14@gmail.com>
2022-09-09 16:07:07 -0300

    1009

--- index.js
+++ index.js
@@ -3,11 +3,16 @@ let valorDoA = Number(document.getElementById("valorA").value);
 let valorDoB = Number(document.getElementById("valorB").value);
 let valorDoC = Number(document.getElementById("valorC").value);
 
-const delta =  valorDoB ** 2 - (4 * valorDoA * valorDoC)
+const delta = Math.pow(valorDoB,2)   -4 * valorDoA * valorDoC
 
 if(delta > 0){
-    const contaUm = (- valorDoB + delta / (2 * valorDoA));
-    const contaDois =  (- valorDoB - delta / (2 * valorDoA));
+
+
+
+    const contaUm = (-valorDoB + Math.sqrt(delta)) / (2 * valorDoA);
+    const contaDois =  (-valorDoB - Math.sqrt(delta)) / (2 * valorDoA);
+
+     
 
     document.getElementById("resultado").innerHTML = `resultado = ${contaUm}, ${contaDois} `
 
